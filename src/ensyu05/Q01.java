902ef3c816efac5b8c49c05810e6b624c082756c
jichan1115 <tmbf22336@stu.tamagawa.ac.jp>
2026-06-30 16:44:55 +0900

    ensyu05らへん

--- src/ensyu05/Q01.java
+++ src/ensyu05/Q01.java
@@ -4,6 +4,12 @@ package ensyu05;
 
 public class Q01 {
     public static void main(String[] args) {
-
+        System.out.println("メソッドを呼び出します");
+        hello();
+        System.out.println("メソッドの呼び出しが終わりました");
+    }
+    public static void hello(){{
+        System.out.println("湊さん、こんにちは");
+    }
     }
 }
